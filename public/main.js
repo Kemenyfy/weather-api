be677fdf02462ead7cf4bf772bd27f59696392c7
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-07 22:05:01 -0400

    Explorer mode done

--- public/main.js
+++ public/main.js
@@ -1,11 +1,8 @@
 const baseUrl = 'https://api.openweathermap.org/data/2.5/' 
 
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
-
 const search = (event) => {
   event.preventDefault()
+  document.querySelector('.currentWeather').textContent = 'Current Weather in:'
   key = 'e903bc8a5448f296f28f3882ea390d9d'
   console.log('searching')
   console.log(event)
@@ -20,9 +17,17 @@ const search = (event) => {
       }
     })
     .then(list => {
-      console.log(list)
-    })
+      console.log(list.name)
+      console.log(list.main.temp)
+      let newLi = document.createElement('li')
+      newLi.textContent = list.name
+      document.querySelector('.currentWeather').appendChild(newLi);
+      newLi = document.createElement('li')
+      newLi.textContent = list.main.temp
+      document.querySelector('.currentWeather').appendChild(newLi);
+      document.querySelector('.currentWeather').style.listStyle = "none"
+    }) 
 }
 
-document.addEventListener('DOMContentLoaded', main)
 document.querySelector('form').addEventListener('submit', search)
+document.querySelector('.checkButton').addEventListener('click', search)
