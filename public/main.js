9d0fb758a57ff132d7423b010e6abe8c7f53337f
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-08 16:39:50 -0400

    Refactored to Add Name

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ class WeatherSearch {
   constructor(placeToSearch) {
     this.location = placeToSearch
   }
-  getResults () {
+  getResults() {
     const baseUrl = 'https://api.openweathermap.org/data/2.5/'
     const key = 'e903bc8a5448f296f28f3882ea390d9d'
     const _url = `${baseUrl}weather?q=${this.location}&appid=${key}&units=metric`
@@ -17,16 +17,28 @@ class WeatherSearch {
       .then(list => {
         console.log(list.name)
         console.log(list.main.temp)
-        let newLi = document.createElement('li')
-        newLi.textContent = list.name
-        document.querySelector('.currentWeather').appendChild(newLi);
-        newLi = document.createElement('li')
-        newLi.textContent = list.main.temp
-        document.querySelector('.currentWeather').appendChild(newLi);
         document.querySelector('.currentWeather').style.listStyle = "none"
+        let object = new AddNameToDom(list.name)
+        object.render()
       })
   }
 }
+
+class AddNameToDom {
+  constructor(variableToAdd) {
+    this.item = variableToAdd
+  }
+  render () {
+    let newLi = document.createElement('li')
+    newLi.textContent = this.item
+    document.querySelector('.currentWeather').appendChild(newLi);
+
+  }
+}
+// TODO: make into own class
+// let newLi = document.createElement('li')
+// newLi.textContent = list.main.temp
+// document.querySelector('.currentWeather').appendChild(newLi);
 
 const search = (event) => {
   event.preventDefault()
